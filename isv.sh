bd4e467de76ec25faa818dac78f786e773b43e75
Noah Hellman <noah.hellman@protonmail.com>
2018-07-27 13:48:32 +0200

    separate sync, list to separate commands

--- isv.sh
+++ isv.sh
@@ -34,49 +34,74 @@ then CFG_DIR="$HOME/.config/ics-schedule-view"
 else CFG_DIR="$XDG_CONFIG_HOME/ics-schedule-view"
 fi
 
-mkdir -p $CFG_DIR
-CALS_FILE=$CFG_DIR/calendars
-ENTRIES=$CCH_DIR/entries
-
+NORMAL_COL="\033[0m"
 [ -z "$ISV_DAY_FMT" ] && ISV_DAY_FMT="%A, %B %d:"
 [ -z "$ISV_TIME_FMT" ] && ISV_TIME_FMT="%H%M"
 [ -z "$ISV_DAY_COL" ] && ISV_DAY_COL="\033[1m"
 [ -z "$ISV_TIME_COL" ] && ISV_TIME_COL="\033[1;32m"
-[ -z "$ISV_SUM_COL" ] && ISV_SUM_COL="\033[0m"
+[ -z "$ISV_SUM_COL" ] && ISV_SUM_COL="$NORMAL_COL"
 
-int_start=$(date -d "monday -1 week" +"%s")
-int_end=$(date -d "monday 1 week" +"%s")
-mkdir -p $RNT_DIR || exit 1
-[ ! -r $CALS_FILE ] && die "no file with calendars found at $CALS_FILE"
-curl -s $(cat $CALS_FILE) | tr -d '\r' > $RNT_DIR/schedule.ics
+CALS_FILE=$CFG_DIR/calendars
+ENTRIES=$CCH_DIR/entries
 
-mkdir -p $CCH_DIR
-AWK_PARSE='BEGIN { FS=":"; OFS="\t" }
-$1 == "DTSTART" { start=$2 }
-$1 == "DTEND" { end=$2 }
-$1 == "SUMMARY" { rs=true; summary=$2 }
-NF == 1 && rs == true { summary=summary substr($1,2) } # read multiline summary
-NF == 2 { rs=false } # colon -> end read summary
-$1 == "END" { print start,end,summary }'
-awk "$AWK_PARSE" $RNT_DIR/schedule.ics |\
-    tr -d '\' 2>/dev/null |\
-    sort > $ENTRIES
-rm -rf $RNT_DIR
+USAGE="usage: isv <command> [<args>]
 
-day_end=0
-while read start end summary; do
-    start_unix=$(date_ics_fmt $start "%s")
-    [ "$int_end" -lt "$start_unix" ] && break
-    if [ "$int_start" -lt "$start_unix" ]; then
-        if [ "$day_end" -lt "$start_unix" ]; then
-            day=$(date -d "@$start_unix" +"%F")
-            day_end=$(date -d "$day 23:59" +"%s")
-            echo -e "\n$ISV_DAY_COL$(date -d "$day" +"$ISV_DAY_FMT")\e[0m"
+commands:
+    sync       s  -- fetch and parse calendars listed in
+                     $CALS_FILE
+    list    ls l  -- display events from calendars"
+
+sync_cmd() {
+    [ ! -r $CALS_FILE ] && die "no file with calendars found at $CALS_FILE"
+    mkdir -p $RNT_DIR
+    curl -s $(cat $CALS_FILE) | tr -d '\r' > $RNT_DIR/schedule.ics
+
+    mkdir -p $CCH_DIR
+    AWK_PARSE='BEGIN { FS=":"; OFS="\t" }
+    $1 == "DTSTART" { start=$2 }
+    $1 == "DTEND" { end=$2 }
+    $1 == "SUMMARY" { rs=true; summary=$2 }
+    NF == 1 && rs == true { summary=summary substr($1,2) } # read multiline summary
+    NF == 2 { rs=false } # colon -> end read summary
+    $1 == "END" { print start,end,summary }'
+    awk "$AWK_PARSE" $RNT_DIR/schedule.ics |\
+        tr -d '\' 2>/dev/null |\
+        sort > $ENTRIES
+    rm -rf $RNT_DIR
+}
+
+list_cmd() {
+    [ -r $ENTRIES ] || die "no cache, use sync command"
+    int_start=$(date -d "monday -1 week" +"%s")
+    int_end=$(date -d "monday 10 week" +"%s")
+    day_end=0
+    while read start end summary; do
+        start_unix=$(date_ics_fmt $start "%s")
+        [ "$int_end" -lt "$start_unix" ] && break
+        if [ "$int_start" -lt "$start_unix" ]; then
+            if [ "$day_end" -lt "$start_unix" ]; then
+                day=$(date -d "@$start_unix" +"%F")
+                day_end=$(date -d "$day 23:59" +"%s")
+                echo -e "$ISV_DAY_COL" \
+                    "\n$(date -d "$day" +"$ISV_DAY_FMT")" \
+                    "$NORMAL_COL"
+            fi
+            start_time=$(date_ics_fmt $start "$ISV_TIME_FMT")
+            end_time=$(date_ics_fmt $end "$ISV_TIME_FMT")
+            echo -e "$ISV_TIME_COL[$start_time-$end_time]" \
+                    "$ISV_SUM_COL$summary" |\
+                fmt -sw 60 |\
+                sed '2,$s/^/            /g'
         fi
-        start_time=$(date_ics_fmt $start "$ISV_TIME_FMT")
-        end_time=$(date_ics_fmt $end "$ISV_TIME_FMT")
-        echo -e "$ISV_TIME_COL[$start_time-$end_time] $ISV_SUM_COL$summary" |\
-            fmt -sw 60 |\
-            sed '2,$s/^/            /g'
-    fi
-done < $ENTRIES | tail -n +2
+    done < $ENTRIES | tail -n +2
+}
+ 
+command=$1
+shift
+[ -z "$command" ] && list_cmd && exit 0
+
+case $command in
+    s|sync) sync_cmd "$@";;
+    l|ls|list) list_cmd "$@";;
+    *) die "invalid command -- $command" "\n\n$USAGE";;
+esac
